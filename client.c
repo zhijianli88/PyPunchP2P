35562bda47936f441c7aebf1b722a982b888c854
Li Zhijian <zhijianli88@163.com>
2015-11-08 22:26:47 +0800

    simple to chat
    Signed-off-by: Li Zhijian <zhijianli88@163.com>

--- client.c
+++ client.c
@@ -23,19 +23,10 @@ int get_nat_type(const char *stun_server, int port)
 
 int main(int argc, char *argv[])
 {
-#if 0
-	if (argc != 4) {
-		printf(USAGE, argv[0]);
-		return -1;
-	}
-
-	int fd = sockfd(AF_INET, SOCK_DGRAM, 0);
-#endif
-
 	struct sockaddr_in server;
 	struct sockaddr_in client;
 	unsigned long dst_ip;
-	int port;
+	int port, nat_type;
 	int s;
 	int n;
 	char buf[BUFSIZE];
@@ -44,7 +35,7 @@ int main(int argc, char *argv[])
 
 	if (argc != 4) {
 		fprintf(stderr, USAGE, argv[0]);
-		//return -1;
+		return -1;
 	}
 
 	if ((dst_ip = inet_addr(argv[1])) == INADDR_NONE) {
@@ -54,7 +45,7 @@ int main(int argc, char *argv[])
 			fprintf(stderr,"gethostbyname error\n");
 			exit(EXIT_FAILURE);
 		}
-		memcpy((char *)&dst_ip,(char *)he->h_addr,he->h_length);
+		memcpy((char *)&dst_ip, (char *)he->h_addr, he->h_length);
 	}
 	
 	if ((port = atoi(argv[2])) == 0) {
@@ -63,8 +54,12 @@ int main(int argc, char *argv[])
 			port=(int)ntohs((u_short)se->s_port);
 		}
 	}
+	if (port <= 0 && port > 65535) {
+		fprintf(stderr, "port %d overflow\n", port);
+		return -1;
+	}
 
-	if((s = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
+	if ((s = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
 		perror("socket");
 		exit(EXIT_FAILURE);
 	}
@@ -73,19 +68,22 @@ int main(int argc, char *argv[])
 	client.sin_addr.s_addr = htonl(INADDR_ANY);
 	client.sin_port = htons(0);
 
-	if(bind(s, (struct sockaddr *)&client, sizeof(client)) < 0) {
+	nat_type = get_nat_type(NULL, 0);
+	sprintf(buf, "%s %d", argv[3], nat_type);
+
+	if (bind(s, (struct sockaddr *)&client, sizeof(client)) < 0) {
 		perror("bind");
 		exit(EXIT_FAILURE);
 	}
 	memset((char *)&server, 0, sizeof(server));
 	server.sin_family = AF_INET;
 	server.sin_addr.s_addr = dst_ip;
-	//inet_pton(AF_INET, argv[DST_IP], &server.sin_addr); 
 	server.sin_port = htons(port);
 	zero = 0;
 
-	sendto(s, "1 1", 3, 0, (struct sockaddr*)&server, sizeof(server));
-	printf("send 1 1\n");
+	sendto(s, buf, strlen(buf), 0, (struct sockaddr*)&server, sizeof(server));
+	//sendto(s, "1 1", 3, 0, (struct sockaddr*)&server, sizeof(server));
+	printf("send %s\n", buf);
 
 	n = recvfrom(s, buf, BUFSIZE - 1, 0, (struct sockaddr*)0, &zero);
 	if (n < 0) {
@@ -101,25 +99,33 @@ int main(int argc, char *argv[])
 		printf("recvfrom error\n");
 		return -1;
 	}
+
+	server.sin_addr.s_addr = *(in_addr_t *)buf;
+	server.sin_port = htons(*((uint16_t *)(buf + 4)));
 	printf("recv %d: %s, %s, %d, %d\n", n, buf,
 			inet_ntoa(*(struct in_addr *)buf), *((uint16_t *)(buf + 4)), *((uint16_t *)(buf + 6)));
 
-	//printf("outof while\n");
-	//printf("%ld",dst_ip);
-	printf("UDP>");
-	fflush(stdout);	
-	while((n = read(0, buf, BUFSIZE)) > 0) {
+
+	sprintf(buf, "sleep..., try to connect...\n");
+	sleep(3);
+	/* connect to peer */
+	sendto(s, buf, strlen(buf), 0, (struct sockaddr*)&server, sizeof(server));
+	sendto(s, buf, strlen(buf), 0, (struct sockaddr*)&server, sizeof(server));
+	printf("%s", buf);
+
+	while ((n = read(0, buf, BUFSIZE)) > 0) {
 		buf[n]='\0';
 		sscanf(buf, "%s", cmd);
 	
-		if(strcmp(cmd, "quit") == 0) {
+		if (strcmp(cmd, "quit") == 0) {
 	 		break;
 		}
-		if(sendto(s, buf, n, 0, (struct sockaddr*)&server, sizeof(server)) < 0) {
+		printf("while send %s\n", buf);
+		if (sendto(s, buf, n, 0, (struct sockaddr*)&server, sizeof(server)) < 0) {
 			break;
 		}
-	
-		if((n = recvfrom(s, buf, BUFSIZE-1, 0, (struct sockaddr*)0, &zero)) < 0)
+
+		if((n = recvfrom(s, buf, BUFSIZE - 1, 0, (struct sockaddr*)0, &zero)) < 0)
 			break;
 
 		buf[n]='\0';
@@ -127,7 +133,7 @@ int main(int argc, char *argv[])
 		printf("UDP>");
 		fflush(stdout);
 	}
-		close(s);
-		return EXIT_SUCCESS;
+	close(s);
+	return EXIT_SUCCESS;
 }
 
